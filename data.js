cace3decb8f6a53ddcbefb5c22fb29fde22241a5
BenjaminUnrauDyck <69878092+BenjaminUnrauDyck@users.noreply.github.com>
2024-09-08 01:31:07 -0400

    Add files via upload

--- data.js
+++ data.js
@@ -1,9 +1,10 @@
+
 export const githubUsername = "BenjaminUnrauDyck";
 
 export const Name = "Benjamin";
 export const LastName = "Unrau Dyck";
 export const FullName = `${Name} ${LastName}`;
-export const mail = "benjaminunrau07@gmail.com`;
+export const mail = "benjaminunrau07@gmail.com";
 
 // Meta information
 export const metaTitle = "Benjamin Unrau Dyck's Portfolio";
@@ -15,23 +16,26 @@ export const metaAuthor = "Benjamin Unrau Dyck";
 export const contactInfo = `I'm just an email or a message away on any platform. Send me an email at ${mail}, or find me on LinkedIn, Twitter, Facebook, or Instagram. Let’s start a conversation about your ideas or just enjoy a casual chat.`;
 
 export const bio = [
-    "I am a 23-year-old Computer Science student with a strong foundation in software development, specializing in backend technologies. I am particularly interested in Quality Assurance (QA), with a growing passion for automation and AI. My multilingual skills in German, English, and Spanish, combined with my growing technical expertise, make me a valuable asset in diverse, collaborative environments.",
+    "Discover Benjamin Unrau Dyck.",
+    `As a Software Developer, I merge a rigorous academic background in Software Engineering with profound real-world experience across development and quality assurance.`,
+    `I excel in harnessing multiple programming languages and automation tools to not only meet but advance industry standards through innovative solutions.`,
+    `Beyond developing software, I'm committed to educating and empowering the next generation of tech professionals, sharing insights and inspiring innovation.`,
+    "Thank you for exploring my journey and contributions to the tech world!",
 ];
 
 export const languages = [
+
     {
         title: "German Communication",
-        skillName: "German",
+        skillName: "German ",
         color: "1",
         percentage: "100",
-    },
-    {
+    }, {
         title: "English Communication",
         skillName: "English",
         color: "2",
         percentage: "100",
-    },
-    {
+    }, {
         title: "Spanish Communication",
         skillName: "Spanish",
         color: "3",
@@ -42,71 +46,161 @@ export const languages = [
 export const skills = {
     "Programming and Scripting Languages": [
         {
-            skillName: "Java",
-            imagePath: "images/skills/java_logo.png",
+            "skillName": "Python",
+            "imagePath": "images/skills/python_logo.png",
         },
         {
-            skillName: "Python",
-            imagePath: "images/skills/python_logo.png",
+            "skillName": "Java",
+            "imagePath": "images/skills/java_logo.png",
         },
-        {
-            skillName: "JavaScript",
-            imagePath: "images/skills/javascript_logo.png",
-        },
-        {
-            skillName: "CSS",
-            imagePath: "images/skills/css_logo.png",
-        },
-        {
-            skillName: "C++",
-            imagePath: "images/skills/c++_logo.png",
-        }
     ],
     "Database Management Systems": [
         {
-            skillName: "MySQL",
-            imagePath: "images/skills/mysql_logo.png",
-            description: "A relational database management system used for storing and retrieving data efficiently."
+            "skillName": "MySQL",
+            "imagePath": "images/skills/mysql_logo.png",
+            "description": "A relational database management system used for storing and retrieving data efficiently."
+        },
+        {
+            "skillName": "MongoDB",
+            "imagePath": "images/skills/mongodb_logo.png",
+            "description": "A NoSQL database used for high-volume data storage."
         }
     ],
     "Version Control and CI/CD Tools": [
         {
-            skillName: "Git",
-            imagePath: "images/skills/git_logo.png",
-            description: "Version control system to track changes in source code during software development."
-        }
+            "skillName": "Git",
+            "imagePath": "images/skills/git_logo.png",
+            "description": "Version control system to track changes in source code during software development."
+        },
     ]
 };
 
+export const certifications = {
+
+
+    educationalCertifications: [
+        {
+            certificationName: "FPUNA Grades 1",
+            image: "images/Certifications/FP_UNA/FPUNA_grades-1.jpg",
+            preview: "https://ivanweissvanderpol.github.io/images/Certifications/FP_UNA/FPUNA_grades-1.jpg"
+        },
+        {
+            certificationName: "FPUNA Grades 2",
+            image: "images/Certifications/FP_UNA/FPUNA_grades-2.jpg",
+            preview: "https://ivanweissvanderpol.github.io/images/Certifications/FP_UNA/FPUNA_grades-2.jpg"
+        },
+        {
+            certificationName: "ISTQB Foundation Level",
+            image: "images/Certifications/Mentormate/Certificate-8e85e79dd27d41078da9908a53aaedb8_page-0001.jpg",
+            preview: "https://ivanweissvanderpol.github.io/images/Certifications/Mentormate/Certificate-8e85e79dd27d41078da9908a53aaedb8_page-0001.jpg"
+        },
+        {
+            certificationName: "Advanced English Proficiency Certificate BECAL Scholarship 2022",
+            image: "images/Certifications/Personal/Ivan_Weiss_Advanced_English_Proficiency_Certificate_BECAL_Scholarship_2022.jpg",
+            preview: "https://ivanweissvanderpol.github.io/images/Certifications/Personal/Ivan_Weiss_Advanced_English_Proficiency_Certificate_BECAL_Scholarship_2022.jpg"
+        },
+        {
+            certificationName: "NASA International Space Apps Challenge",
+            image: "images/Certifications/Personal/NASA_International_Space_Apps_Challenge.jpg",
+            preview: "https://ivanweissvanderpol.github.io/images/Certifications/Personal/NASA_International_Space_Apps_Challenge.jpg"
+        },
+        {
+            certificationName: "Agile Course Certificate",
+            image: "images/Certifications/Personal/Agile_Course_certificate.jpg",
+            preview: "https://ivanweissvanderpol.github.io/images/Certifications/Personal/Agile_Course_certificate.jpg"
+        }
+    ]
+}
+
+
 export const experience = [
     {
-        title: "WPG.srl",
-        duration: "April 2023 - Present",
-        subtitle: "Internship",
+        title: "MentorMate",
+        subtitle: "Quality Specialist II - Professional Development and Training",
+        duration: "August 2023 - Present",
         details: [
-            "Worked on a project utilizing OpenAI-Azure integration to develop a personalized AI coding assistant based on company guidelines.",
-            "Conducted research on fine-tuning AI models and optimizing performance.",
-            "Applied prompt engineering and instruction set development for enhanced AI functionalities."
+            "Engaged in a structured professional development program, enhancing knowledge in QA methodologies and compliance standards.",
+            "Participated in multiple training programs and workshops, gaining proficiency in new tools and technologies including Postman, Charles Proxy, and MongoDB.",
+            "Actively involved in global teams across Paraguay, USA, and Bulgaria, enhancing cross-functional and cross-cultural collaboration languages.",
+            "helped in the trainings of bootcamps covering API Tesing using postman."
         ],
-        tags: ["AI", "OpenAI", "Azure", "Prompt Engineering", "QA"],
+        tags: ["Professional Development", "Regulatory Compliance", "QA Methodologies", "Global Collaboration", "Healthcare Software"],
         icon: "briefcase",
+    },
+    {
+        title: "MentorMate",
+        subtitle: "Quality Specialist II - Flyte Project",
+        duration: "August  2023 - Present",
+        details: [
+            "Oversaw quality assurance for the Flyte Project, focusing on a webiste, a mobile application and an FDA-cleared device to improve women's pelvic health.",
+            "Collaborated with developers and project managers to ensure seamless integration of new features, maintaining high-quality user experiences.",
+            "Managed detailed documentation efforts to ensure compliance with industry standards and facilitated effective team and stakeholder communication.",
+            "Developed test plans and strategies that ensured product reliability."
+        ],
+        tags: ["Flyte Project", "FDA Compliance", "Quality Assurance", "Test Automation", "Project Documentation"],
+        icon: "heartbeat",
+    },
+    {
+        title: "WPG.srl",
+        duration: "October 2018 - December 2022",
+        subtitle: "QA Engineer and Software Developer",
+        details: [
+            "Performed comprehensive manual and automated testing, ensuring all new software applications met stringent quality standards before release.",
+            "Contributed to various phases of the software development lifecycle, enhancing system designs and functionality with innovative solutions.",
+            "Developed and maintained critical documentation for testing processes and software functionalities, significantly improving future maintenance and upgrades.",
+            "Created a Python-based automation tools to streampline the test data generation and and processing for reports."
+        ],
+        tags: ["Software Testing", "Software Development", "Automation", "Machine Learning", "Documentation"],
+        icon: "globe",
     }
 ];
 
 export const education = [
     {
         title: "Bachelor’s Degree in Computer Science",
-        duration: "July 2021 - Present",
+        duration: "July 2021 - present",
         subtitle: "Facultad Politécnica de la Universidad Nacional de Asunción (FP-UNA)",
         details: [
-            "Pursuing a curriculum covering database management, object-oriented programming, and software development methodologies.",
-            "Participated in academic projects, demonstrating leadership and problem-solving skills.",
-            "Volunteered at university events, gaining hands-on experience in event management and technical support."
+            "Completed a rigorous curriculum encompassing a wide range of computer science disciplines from fundamental theories to practical applications.",
+            "Engaged in academic competitions and collaborative certifications that enhanced languages in teamwork, problem-solving, and innovation, achieving top placements in national and international contests.",
+            "Developed proficiency in a variety of programming languages and technical tools through intensive coursework and self-directed learning, preparing for a seamless transition to professional environments.",
+            "Cultivated strong communication and leadership languages through active participation in student councils and as a student mentor, helping to organize major campus events and workshops."
         ],
-        tags: ["Algorithms", "Software Engineering", "Programming Languages", "Web Development", "Team Leadership"],
+        tags: [
+            "Algorithms & Data Structures",
+            "Software Engineering",
+            "Machine Learning",
+            "Data Security",
+            "Operating Systems",
+            "Database Management Systems",
+            "Computer Networks",
+            "Compiler Design",
+            "Web Development",
+            "Network Security",
+            "Programming Languages",
+            "Front-End Development",
+            "Back-End Development",
+            "Project Management",
+            "Compiler Design",
+            "Probabilities & Statistics",
+            "Physics",
+            "Economics and Finances",
+            "Investigation of Operations",
+            "Technical Writing",
+        ],
         icon: "graduation-cap",
-    }
+    },
 ];
+
+export const testimonials = {
+    reviewer: "Anonymous Colleagues",
+    period: "Six-Month Review",
+    company: "MentorMate",
+    feedback: [
+
+    ]
+  };
+  
 
 export const footer = [
     {
@@ -115,19 +209,19 @@ export const footer = [
             {
                 text: "GitHub",
                 link: "https://github.com/BenjaminUnrauDyck",
-            }
+            },
         ],
     },
     {
         label: "Resources",
         data: [
             {
-                text: "Send me a mail",
+                text: "send me a mail",
                 link: "mailto:benjaminunrau07@gmail.com",
             },
             {
                 text: "Download Resume",
-                link: "assets\\Benjamin_Unrau.pdf",
+                link: "assets\\Ivan_Weiss_Resume.pdf",
                 target: "_blank"
             }
         ],
@@ -136,20 +230,24 @@ export const footer = [
         label: "Social Profiles",
         data: [
             {
-                text: "LinkedIn",
+                text: "Linkedin",
                 link: "https://www.linkedin.com/in/benjamin-unrau-dyck/",
             },
             {
-                text: "WhatsApp",
+                text: "whatsapp",
                 link: "https://wa.me/595986361808",
-            }
+            },
+            {
+                text: "Instagram",
+                link: "https://www.instagram.com/ivan_weiss_van_der_pol/",
+            },
         ],
     },
     {
-        label: "Copyright Text",
+        label: "copyright-text",
         data: [
-            "Made with &hearts; by Benjamin Unrau Dyck.",
-            "&copy; No Copyrights. Feel free to use this template."
+            "Made with &hearts; by BenjaminUnrauDyck.",
+            "&copy; No Copyrights. Feel free to use this template.",
         ],
-    }
+    },
 ];
