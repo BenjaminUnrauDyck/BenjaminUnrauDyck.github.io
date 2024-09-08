cf5e8e3570888c7067dc8ea96ee598b51a739db0
Benjamin Unrau Dyck <69878092+BenjaminUnrauDyck@users.noreply.github.com>
2024-09-08 13:16:22 -0400

    Add files via upload

--- data.js
+++ data.js
@@ -17,9 +17,9 @@ export const contactInfo = `I'm just an email or a message away on any platform.
 
 export const bio = [
     "Discover Benjamin Unrau Dyck.",
-    `As a Software Developer, I merge a rigorous academic background in Software Engineering with profound real-world experience across development and quality assurance.`,
-    `I excel in harnessing multiple programming languages and automation tools to not only meet but advance industry standards through innovative solutions.`,
-    `Beyond developing software, I'm committed to educating and empowering the next generation of tech professionals, sharing insights and inspiring innovation.`,
+    `I am a 23-year-old Computer Science student with a strong foundation in software development, specializing in backend technologies.`,
+`I have a keen interest in AI, cybersecurity, and open source technology.`,
+`My multilingual skills in German, English, and Spanish, combined with my growing technical expertise, make me a valuable asset in diverse, collaborative environments.`,
     "Thank you for exploring my journey and contributions to the tech world!",
 ];
 
@@ -56,15 +56,10 @@ export const skills = {
     ],
     "Database Management Systems": [
         {
-            "skillName": "MySQL",
-            "imagePath": "images/skills/mysql_logo.png",
+            "skillName": "PostgreSQL",
+            "imagePath": "images/skills/Postgresql_elephant.svg",
             "description": "A relational database management system used for storing and retrieving data efficiently."
         },
-        {
-            "skillName": "MongoDB",
-            "imagePath": "images/skills/mongodb_logo.png",
-            "description": "A NoSQL database used for high-volume data storage."
-        }
     ],
     "Version Control and CI/CD Tools": [
         {
@@ -88,72 +83,30 @@ export const certifications = {
             certificationName: "FPUNA Grades 2",
             image: "images/Certifications/FP_UNA/FPUNA_grades-2.jpg",
             preview: "https://ivanweissvanderpol.github.io/images/Certifications/FP_UNA/FPUNA_grades-2.jpg"
-        },
+        },*/
         {
-            certificationName: "ISTQB Foundation Level",
-            image: "images/Certifications/Mentormate/Certificate-8e85e79dd27d41078da9908a53aaedb8_page-0001.jpg",
-            preview: "https://ivanweissvanderpol.github.io/images/Certifications/Mentormate/Certificate-8e85e79dd27d41078da9908a53aaedb8_page-0001.jpg"
+            certificationName: "Introduccion al Aseguramiento de la Calidad y Automatizacion",
+            image: "images/Certifications/bFP_UNA/Benjamin Unrau.jpg",
+            preview: "https://benjaminunraudyck.github.io/images/Certifications/bFP_UNA/Benjamin Unrau.jpg.jpg"
         },
-        {
-            certificationName: "Advanced English Proficiency Certificate BECAL Scholarship 2022",
-            image: "images/Certifications/Personal/Ivan_Weiss_Advanced_English_Proficiency_Certificate_BECAL_Scholarship_2022.jpg",
-            preview: "https://ivanweissvanderpol.github.io/images/Certifications/Personal/Ivan_Weiss_Advanced_English_Proficiency_Certificate_BECAL_Scholarship_2022.jpg"
-        },
-        {
-            certificationName: "NASA International Space Apps Challenge",
-            image: "images/Certifications/Personal/NASA_International_Space_Apps_Challenge.jpg",
-            preview: "https://ivanweissvanderpol.github.io/images/Certifications/Personal/NASA_International_Space_Apps_Challenge.jpg"
-        },
-        {
-            certificationName: "Agile Course Certificate",
-            image: "images/Certifications/Personal/Agile_Course_certificate.jpg",
-            preview: "https://ivanweissvanderpol.github.io/images/Certifications/Personal/Agile_Course_certificate.jpg"
-        }*/
     ]
 }
 
 
 export const experience = [
-/*    {
-        title: "MentorMate",
-        subtitle: "Quality Specialist II - Professional Development and Training",
-        duration: "August 2023 - Present",
-        details: [
-            "Engaged in a structured professional development program, enhancing knowledge in QA methodologies and compliance standards.",
-            "Participated in multiple training programs and workshops, gaining proficiency in new tools and technologies including Postman, Charles Proxy, and MongoDB.",
-            "Actively involved in global teams across Paraguay, USA, and Bulgaria, enhancing cross-functional and cross-cultural collaboration languages.",
-            "helped in the trainings of bootcamps covering API Tesing using postman."
-        ],
-        tags: ["Professional Development", "Regulatory Compliance", "QA Methodologies", "Global Collaboration", "Healthcare Software"],
-        icon: "briefcase",
-    },
     {
-        title: "MentorMate",
-        subtitle: "Quality Specialist II - Flyte Project",
-        duration: "August  2023 - Present",
+        title: "WPG S.R.L",
+        duration: "May 2024 - Present",
+        subtitle: "Internship",
         details: [
-            "Oversaw quality assurance for the Flyte Project, focusing on a webiste, a mobile application and an FDA-cleared device to improve women's pelvic health.",
-            "Collaborated with developers and project managers to ensure seamless integration of new features, maintaining high-quality user experiences.",
-            "Managed detailed documentation efforts to ensure compliance with industry standards and facilitated effective team and stakeholder communication.",
-            "Developed test plans and strategies that ensured product reliability."
-        ],
-        tags: ["Flyte Project", "FDA Compliance", "Quality Assurance", "Test Automation", "Project Documentation"],
-        icon: "heartbeat",
-    },
-    {
-        title: "WPG.srl",
-        duration: "October 2018 - December 2022",
-        subtitle: "QA Engineer and Software Developer",
-        details: [
-            "Performed comprehensive manual and automated testing, ensuring all new software applications met stringent quality standards before release.",
-            "Contributed to various phases of the software development lifecycle, enhancing system designs and functionality with innovative solutions.",
-            "Developed and maintained critical documentation for testing processes and software functionalities, significantly improving future maintenance and upgrades.",
-            "Created a Python-based automation tools to streampline the test data generation and and processing for reports."
+            "Worked on a project utilizing OpenAI-Azure integration to develop a personalized AI coding assistant based on company guidelines and code.",
+            "Conducted research on fine-tuning AI models and optimizing performance.",
+            "Learned about prompt engineering and instruction set development for enhanced AI functionalities, and maximising precision.",
         ],
         tags: ["Software Testing", "Software Development", "Automation", "Machine Learning", "Documentation"],
         icon: "globe",
     }
-];*/
+];
 
 export const education = [
     {
@@ -161,11 +114,11 @@ export const education = [
         duration: "July 2021 - present",
         subtitle: "Facultad Politécnica de la Universidad Nacional de Asunción (FP-UNA)",
         details: [
-            /*"Completed a rigorous curriculum encompassing a wide range of computer science disciplines from fundamental theories to practical applications.",
-            "Engaged in academic competitions and collaborative certifications that enhanced languages in teamwork, problem-solving, and innovation, achieving top placements in national and international contests.",
-            "Developed proficiency in a variety of programming languages and technical tools through intensive coursework and self-directed learning, preparing for a seamless transition to professional environments.",
-            "Cultivated strong communication and leadership languages through active participation in student councils and as a student mentor, helping to organize major campus events and workshops."
-        ],*/
+            "Currently pursuing a diverse curriculum covering essential computer science fields, including database management, object-oriented programming, and software development methodologies.",
+            "Led and participated in various academic projects, demonstrating strong leadership and problem-solving skills in real-world scenarios, and fostering team collaboration.",
+            "Participated and volunteered at university events, contributing as a helper and organizer, gaining hands-on experience in event management and technical support, enhancing teamwork and communication skills in fast-paced, dynamic environments.",
+            "Gained practical experience with multiple programming languages and technologies through coursework and self-directed learning, including SQL, Python, Java.",
+        ],
         tags: [
             "Algorithms & Data Structures",
             "Software Engineering",
@@ -219,11 +172,11 @@ export const footer = [
                 text: "send me a mail",
                 link: "mailto:benjaminunrau07@gmail.com",
             },
-           /* {
+            {
                 text: "Download Resume",
-                link: "assets\\Ivan_Weiss_Resume.pdf",
+                link: "assets\\Benjamin_UnrauDyck.pdf",
                 target: "_blank"
-            }*/
+            }
         ],
     },
     {
@@ -234,8 +187,12 @@ export const footer = [
                 link: "https://www.linkedin.com/in/benjamin-unrau-dyck/",
             },
             {
-                text: "whatsapp",
+                text: "Whatsapp",
                 link: "https://wa.me/595986361808",
+            },
+            {
+                text: "Instagram",
+                link: "https://www.instagram.com/ivan_weiss_van_der_pol/",
             },
         ],
     },
